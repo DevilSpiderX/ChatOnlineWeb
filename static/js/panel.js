1b451a9faf4056d1c76f85814799ded042a2f0c1
DevilSpiderX <775716441@qq.com>
2021-12-03 00:13:38 +0800

    2021.12.03 00:13:00 1.网页后台代码完成

--- static/js/panel.js
+++ static/js/panel.js
@@ -1,25 +1,15 @@
 const own_uid = getQueryVariable("uid");
 const token = getQueryVariable("token");
 const ws = new WebSocket("ws://" + location.host + "/websocket/" + own_uid + "/" + token);
+const friendsInfo = {};
+const messageRecord = {};
 
-initHtml();
-
-function getQueryVariable(variable) {
-    let query = window.location.search.substring(1);
-    let vars = query.split("&");
-    for (let i = 0; i < vars.length; i++) {
-        let pair = vars[i].split("=");
-        if (pair[0] === variable) {
-            return pair[1];
-        }
-    }
-    return "null";
-}
+init();
 
 /**
  * 初始化面板
  */
-function initHtml() {
+function init() {
     getInformation(own_uid, function (data) {
         switch (data["code"]) {
             case "0" : {
@@ -34,10 +24,63 @@ function initHtml() {
             }
         }
     });
+    getFriends(own_uid, function (data) {
+        switch (data["code"]) {
+            case "0" : {
+                for (let friend of data["msg"]) {
+                    let friend_uid = friend["friend_uid"];
+                    friendsInfo[friend_uid] = {};
+                    messageRecord[friend_uid] = [];
+                    getInformation(friend_uid, function (data) {
+                        switch (data["code"]) {
+                            case "0": {
+                                friendsInfo[friend_uid]["nick"] = data["msg"]["nick"];
+                                friendsInfo[friend_uid]["age"] = data["msg"]["age"];
+                                friendsInfo[friend_uid]["gender"] = data["msg"]["gender"];
+                                friendsInfo[friend_uid]["intro"] = data["msg"]["intro"];
+                                break;
+                            }
+                            case "1": {
+                                alert(data["msg"]);
+                                break;
+                            }
+                            case "3" : {
+                                alert(data["msg"]);
+                                window.location = "./login.html";
+                                break;
+                            }
+                        }
+                    });
+                }
+                break;
+            }
+            case "1": {
+                console.log(data["msg"]);
+                break;
+            }
+            case "3" : {
+                alert(data["msg"]);
+                window.location = "./login.html";
+                break;
+            }
+        }
+    });
     ws.onopen = wsOnOpen;
     ws.onclose = wsOnClose;
     ws.onerror = wsOnError;
     ws.onmessage = wsOnMessage;
+}
+
+function getQueryVariable(variable) {
+    let query = window.location.search.substring(1);
+    let vars = query.split("&");
+    for (let i = 0; i < vars.length; i++) {
+        let pair = vars[i].split("=");
+        if (pair[0] === variable) {
+            return pair[1];
+        }
+    }
+    return "null";
 }
 
 /* ↓↓这一块是写POST的方法的↓↓ */
@@ -129,12 +172,13 @@ function getFriends(uid, success) {
 
 /* ↓↓这一块是写WebSocket的方法的↓↓ */
 function wsOnOpen(ev) {
-    console.log(ev);
+    // console.log(ev);
     console.log("WebSocket成功接入服务器");
 }
 
 function wsOnClose() {
     console.log("连接已关闭");
+    window.location = "./login.html";
 }
 
 function wsOnError(ev) {
@@ -148,21 +192,54 @@ function wsOnMessage(msgEv) {
     console.log(data);
     switch (data["cmd"]) {
         case "forcedOffline": {
-
+            if (data["code"] === "0") {
+                alert(data["msg"]);
+                window.location = "./login.html";
+            }
             break;
         }
         case "acceptMessage": {
-
+            if (data["code"] === "0") {
+                messageRecord[data["sender_uid"]].push(
+                    {
+                        "msg": data["msg"],
+                        "time": new Date(data["time"]),
+                        "sender_uid": data["sender_uid"]
+                    }
+                );
+            }
             break;
         }
         case "sendMessage": {
-
+            switch (data["code"]) {
+                case "0": {
+                    console.log(data["msg"]);
+                    break;
+                }
+                case "4" :
+                case "5": {
+                    alert("发送失败，" + data["msg"]);
+                    break;
+                }
+                case "500" : {
+                    alert(data["msg"]);
+                    window.location = "./login.html";
+                    break;
+                }
+            }
             break;
         }
     }
 }
 
 function sendMessage(receiver_uid, message) {
+    messageRecord[receiver_uid].push(
+        {
+            "msg": message,
+            "time": new Date(),
+            "sender_uid": own_uid
+        }
+    );
     let data = {
         "cmd": "sendMessage",
         "receiver_uid": receiver_uid,
